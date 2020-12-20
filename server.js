e801ea0b1b95e4b62f309d423f3610afa64f9604
shaggy <simmons5family@gmail.com>
2020-12-19 22:59:30 -0600

    first question (va departments)

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-    //  START ART
-var figlet = require('figlet');
-        // isometric3  poison  slant  speed
-figlet.text('Employee Manager', {
-    font: 'isometric3',
-    horizontalLayout: 'default',
-    verticalLayout: 'default',
-    width: 400,
-    whitespaceBreak: true
-}, function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data);
-});
+//     //  START ART
+// var figlet = require('figlet');
+//         // isometric3  poison  slant  speed
+// figlet.text('Employee Manager', {
+//     font: 'isometric3',
+//     horizontalLayout: 'default',
+//     verticalLayout: 'default',
+//     width: 400,
+//     whitespaceBreak: true
+// }, function(err, data) {
+//     if (err) {
+//         console.log('Something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data);
+// });
+
+const inquirer = require('inquirer');
+
+function start() {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'startOptions',
+            message: 'What would you like to do?',
+            choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Role', 'add an Employee', 'Update an Employees Role']
+        }
+    ]);
+};
+start()
