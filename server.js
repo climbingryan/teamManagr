397fd713d8bd75956136bef294ed2ea8034140e9
shaggy <simmons5family@gmail.com>
2020-12-19 23:53:33 -0600

    all prompts w/ call back to start as y/n

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-//     //  START ART
-// var figlet = require('figlet');
-//         // isometric3  poison  slant  speed
-// figlet.text('Employee Manager', {
-//     font: 'isometric3',
-//     horizontalLayout: 'default',
-//     verticalLayout: 'default',
-//     width: 400,
-//     whitespaceBreak: true
-// }, function(err, data) {
-//     if (err) {
-//         console.log('Something went wrong...');
-//         console.dir(err);
-//         return;
-//     }
-//     console.log(data);
-// });
+    //  START ART
+var figlet = require('figlet');
+        // isometric3  poison  slant  speed
+figlet.text('Employee Manager', {
+    font: 'isometric3',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 400,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
 
 const inquirer = require('inquirer');
 
@@ -24,8 +24,113 @@ function start() {
             type: 'list',
             name: 'startOptions',
             message: 'What would you like to do?',
-            choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Role', 'add an Employee', 'Update an Employees Role']
+            choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employees Role']
         }
-    ]);
+    ]).then(data => {
+            // view FUNCTIONS
+        if (data.startOptions === 'View all Departments') {
+            return viewDepartments();
+        } else if (data.startOptions === 'View all Roles') {
+            return viewRoles();
+        } else if (data.startOptions === 'View all Employees') {
+            return viewEmployees();
+        }   // ADD FUNCTIONS
+        else if (data.startOptions === 'Add a Department') {
+            return addDepartment()
+        } else if(data.startOptions === 'Add a Role') {
+            return addRole();
+        } else if (data.startOptions === 'Add an Employee') {
+            return addEmployee();
+        }    // UPDATE FUNCTIONS 
+        else if (data.startOptions === 'Update an Employees Role') {
+            return updateEmployee();
+        }
+    });
 };
-start()
+
+
+function addDepartment() {
+    return inquirer.prompt([
+        {
+            type: 'text',
+            name: 'addDepartmentName',
+            message: 'Please the name of the department'
+        },
+        {
+            type: 'confirm',
+            name: 'backToStart',
+            message: 'Would you like to go back to the starting menu?'
+        }
+    ]).then( data => {
+        if (data.backToStart) {
+            return start()
+        } 
+    });
+};
+function addRole() {
+    return inquirer.prompt([
+        {
+            type: 'text',
+            name: 'addRoleName',
+            message: 'Please enter the name of the role'
+        },
+        {
+            type: 'text',
+            name: 'addRolesalary',
+            message: 'What is the salary of the role'
+        },
+        {
+            type: 'text',
+            name: 'addRoleDepartment',
+            message: 'Please enter the department of the role'
+        },
+        {
+            type: 'confirm',
+            name: 'backToStart',
+            message: 'Would you like to go back to the starting menu?'
+        }
+    ]).then( data => {
+        if (data.backToStart) {
+            return start()
+        } 
+    });
+};
+function addEmployee() {
+    return inquirer.prompt([
+        {
+            type: 'text',
+            name: 'addEmployeeFirstName',
+            message: 'What is the first name of the employee?'
+        },
+        {
+            type: 'text',
+            name: 'addEmployeeLastName',
+            message: 'What is the last name of the employee?'
+        },
+        {
+            type: 'text',
+            name: 'addEmployeeRole',
+            message: 'What is the role of the employee?'
+        },
+        {
+            type: 'text',
+            name: 'addEmployeeManager',
+            message: 'What is the manager of the employee?'
+        },
+        {
+            type: 'confirm',
+            name: 'backToStart',
+            message: 'Would you like to go back to the starting menu?'
+        }
+    ]).then( data => {
+        if (data.backToStart) {
+            return start()
+        } 
+    });
+};
+
+function updateEmployee() {
+    
+}
+
+start();
