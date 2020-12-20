a70b7be63b3342cf2095d1595038bb2802962b3b
shaggy <simmons5family@gmail.com>
2020-12-19 21:20:06 -0600

    va employees function

--- routes/viewAll/viewAll.js
+++ routes/viewAll/viewAll.js
@@ -34,3 +34,4 @@ viewAllRoles = () => {
     );
 };
 viewAllRoles();
+
--- server.js
+++ server.js
@@ -32,3 +32,13 @@ connection.connect(err => {
     if (err) throw err;
 });
 
+viewAllEmployees = () => {
+    const query = connection.query(
+        `SELECT * FROM employee`,
+        function(err, res) {
+            if (err) throw err;
+            console.table(res);
+        }
+    );
+};
+viewAllEmployees();
